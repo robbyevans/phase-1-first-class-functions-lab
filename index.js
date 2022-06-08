aefe23551c7140b9eb7574abaf5a3fe9a88b5240
Evans Rutto <robbyevans001@gmail.com>
2022-06-08 15:20:12 +0300

    commit tree b79c091bd459cb9c98173031a3b15e68f685d927

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+let array=['don','jose','mike','tommy']
+
+const returnFirstTwoDrivers=function(array){
+    return ([array[0] , array[1]])
+}
+
+
+const returnLastTwoDrivers=function(array){
+    return([array[2],array[3]]);
+}
+returnLastTwoDrivers(array)
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return(function fareMultiplier(fare){
+        return fare *int;
+    })
+}
+const fareDoubler=function(fare){
+    return (fare *2);
+}
+
+const fareTripler =function(fare){
+    return (fare*3)
+}
+
+function selectDifferentDrivers(array,command){
+    if (command===returnFirstTwoDrivers){
+        return(returnFirstTwoDrivers(array));
+    }
+    else if(command===returnLastTwoDrivers){
+        return (returnLastTwoDrivers(array));
+    }
+}
